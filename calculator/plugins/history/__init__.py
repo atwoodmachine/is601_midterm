045a65d432416bc87293983d0571a53985fa81ed
Seniz <so9@njit.edu>
2025-03-10 21:23:05 -0400

    Fix: handles empty history correctly without error

--- calculator/plugins/history/__init__.py
+++ calculator/plugins/history/__init__.py
@@ -5,7 +5,7 @@ from calculator.historyManager import HistoryManager
 class HistoryCommand(Command):
     def execute(self):
         df_history = HistoryManager.get_history_as_df()
-        if df_history is not None:
+        if df_history is not None and not df_history.empty:
             print(df_history.to_string())
         else:
             print("History is empty")
