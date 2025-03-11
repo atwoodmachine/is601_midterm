9bbc3216de3c95b2fef8ddf60fba7dbf58222a38
Seniz <so9@njit.edu>
2025-03-11 13:25:18 -0400

    Fix: better formatting for arguments for readability in history

--- calculator/historyManager/__init__.py
+++ calculator/historyManager/__init__.py
@@ -5,6 +5,7 @@ import pandas as pd
 class HistoryManager:
     HISTORY_DIR = './history'
     HISTORY_FILE = 'calculation_history.csv'
+   
     @classmethod
     def initialize_history(cls):
         if not os.path.exists(cls.HISTORY_DIR):
@@ -13,9 +14,12 @@ class HistoryManager:
         elif not os.access(cls.HISTORY_DIR, os.W_OK):
             logging.error(f"The directory {cls.HISTORY_DIR} is not writeable")
             return
+   
     @classmethod
     def add_to_history(cls, command_name:str, args, result):
-        calc = {'Operation': [command_name], 'Arguments': [args], 'Result': [result]}
+        display_args = [float(arg) for arg in args]
+
+        calc = {'Operation': [command_name], 'Arguments': [display_args], 'Result': [result]}
         df_calc = pd.DataFrame(calc)
         csv_file_path = os.path.join(cls.HISTORY_DIR, cls.HISTORY_FILE)
         if os.path.exists(csv_file_path):
@@ -23,6 +27,7 @@ class HistoryManager:
         else:
             logging.info(f"Created history file {cls.HISTORY_FILE}")
             df_calc.to_csv(csv_file_path, mode='w', header=True, index=False)
+
     @classmethod
     def get_history_as_df(cls):
         csv_file_path = os.path.join(cls.HISTORY_DIR, cls.HISTORY_FILE)
@@ -35,6 +40,7 @@ class HistoryManager:
             logging.error(f"An unexpected error occurred: {e}")
 
         return None
+
     @classmethod
     def get_history_path(cls):
         return os.path.join(cls.HISTORY_DIR, cls.HISTORY_FILE)
