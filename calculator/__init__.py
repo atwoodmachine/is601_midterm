9011e1a5d90d962207287d88d7ac1e40d3e829ed
Seniz <so9@njit.edu>
2025-03-12 22:25:47 -0400

    Fix: removed unnecessary line

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -25,7 +25,6 @@ class Calculator:
 
     def configure_logging(self):
         logging_conf_path = os.getenv('LOGGING_CONF_PATH', 'logging.conf')
-        #logging_conf_path = 'logging.conf'
         if os.path.exists(logging_conf_path):
             logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
         else:
